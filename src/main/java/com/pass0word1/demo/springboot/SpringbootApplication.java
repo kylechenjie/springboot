85878e65388d31b438dc6557229d78e4ad9691e6
kyle <kylechenjie@gmail.com>
2019-01-02 14:29:33 +0800

    first commit

---
+++ src/main/java/com/pass0word1/demo/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.pass0word1.demo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
+
